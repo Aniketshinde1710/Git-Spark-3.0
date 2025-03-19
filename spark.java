73f610e38e85badad0acc6902ae77e7bc055737f
Aniket Shinde <aniketshinde1710@gmail.com>
2025-03-19 17:46:51 +0530

    auth added

--- spark.java
+++ spark.java
@@ -2,7 +2,10 @@ class spark{
     public static void main(String[] args) {
         System.out.println("hello");
     }
-    public void m1(){
+    public void payment() {
         System.out.println("m1");
+    }
+    public void auth() {
+        System.out.println("auth");
     }
 }
