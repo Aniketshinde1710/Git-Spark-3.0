ee849caa808284f0c5b88f99d99e98c9c8bba30e
Aniket Shinde <aniketshinde1710@gmail..com>
2025-03-19 16:49:05 +0530

    notification service added

--- spark.java
+++ spark.java
@@ -1,8 +1,15 @@
-class spark{
+class spark {
     public static void main(String[] args) {
         System.out.println("hello");
     }
-    public void m1(){
+
+    public void m1() {
         System.out.println("m1");
+    }
+
+    public void notifyUser()
+    {
+        System.out.println("hello user");
+
     }
 }
